7e6e7d26f376733edd19376e16d7fec2d662843a
Dan Sowter <dan@netengine.com.au>
2013-12-08 09:36:38 +1000

    Shares progres on groups

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  check_authorization
+  check_authorization :unless => :devise_controller?
 
   # TODO: uncomment this when Facebook authentication is working!
   # before_filter :authenticate_user!

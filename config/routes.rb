0f465d6a7c2ac60ffb8c4b75fa09eaef53697c34
Lucas Caton <lucascaton@gmail.com>
2013-12-07 18:24:54 +1000

    Improving API routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ RhokBrisbane2013::Application.routes.draw do
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  namespace :api do
+  namespace :api, constraints: { format: /json/ } do
     namespace :v1 do
-      resources :resources, constraints: { format: /json/ }
+      resources :resources
     end
   end
 end

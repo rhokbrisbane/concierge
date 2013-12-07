30bc1938278cb577d9fe70e6f0cffa9495a63a7c
Brad Parker <hi@bradparker.com>
2013-12-08 09:40:54 +1000

    Lucas' suggestion

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-50.times do 
-  FactoryGirl.create(:tag)
-end
+FactoryGirl.create_list :tag, 50

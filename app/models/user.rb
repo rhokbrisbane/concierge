febe2a128841753530e984b3c01f4d3b09323a64
skyvalve <blacksmith3d.chris@gmail.com>
2013-12-08 09:41:11 +1000

    Update user.rb
     # Been trying to settling line 15 for Titanium implementation.
     # So far I found that it is very easy to import the whole project
     # into Titanium then later on with certain tweaks should be able 
     # to export for mobile web application from Titanium IDE.
     # According to Dan's comment might be because of Ruby language
     # version are not compatible, I suggest import this project to 
     # Titanium framework and see for yourself to whom interesting 
     # in multi-platforms exportation. Thanks, Christopherys.

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,9 @@ class User < ActiveRecord::Base
 
   has_many :taggings, as: :taggable
   has_many :tags, through: :taggings
-
+ 
+  # This line needed to pay attention for import to Titanium for
+  # later export to multi-platforms, iOS, Android, BlackBerry, Tizen, etc.
   before_save :set_default_name, if: -> (user) { user.name.blank? }
 
   def self.find_for_facebook_oauth(auth, signed_in_resource = nil)

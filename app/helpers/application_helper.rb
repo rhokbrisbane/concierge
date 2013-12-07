795d89122015bf3fafaa8d61820f2b471f3cfbab
Rowan Hogan <rowan.hogan@gmail.com>
2013-12-07 21:35:42 +1000

    Moved login options to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def login_options
+    if signed_in?
+      content_tag(:span, current_user.email, class: "current-user-email") +
+      link_to('Sign Out', destroy_user_session_path, method: :delete, class: "icon-exit button")
+    else
+      render "layouts/authentication_links"
+    end
+  end
 end
+
+

9b33511b8f09d71df16a6b7181fbdb545600573c
Lucas Caton <lucascaton@gmail.com>
2013-12-08 11:12:20 +1000

    Including user_id attribute to SavedSearch

--- app/models/saved_search.rb
+++ app/models/saved_search.rb
@@ -1,4 +1,6 @@
 class SavedSearch < ActiveRecord::Base
   has_many :taggings, as: :taggable
   has_many :tags, through: :taggings
+
+  belongs_to :user, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131208003055) do
+ActiveRecord::Schema.define(version: 20131208010313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 20131208003055) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "taggings", force: true do |t|

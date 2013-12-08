c998923ea9e284c015611f8693e633c6da76aaec
Olivier Pichon <olivier@netengine.com.au>
2013-12-08 15:08:29 +1000

    Adds the possibility to add tags for a user with cancan

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :add_tag]
+  load_and_authorize_resource
 
   def show
     @tags = Tag.all.reject{|tag| @user.tags.include? tag}
@@ -9,11 +9,4 @@ class UsersController < ApplicationController
     tag = Tag.find params[:tag_id]
     @user.tags << tag
   end
-
-  private
-
-  def set_user
-    @user = User.find(params[:id])
-  end
-
 end

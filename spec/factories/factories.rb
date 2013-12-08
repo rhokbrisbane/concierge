b6cf386f75b22ae29fba4e64c39ef083d82736cf
Ricardo Bernardeli <ricardobcs@gmail.com>
2013-12-08 12:16:46 +1000

    Fixes failing tag test.

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -14,10 +14,8 @@ FactoryGirl.define do
     addressable { create(:user) }
   end
 
-  categories = Tag::CATEGORIES
-
   factory :tag do
     name { Faker::Lorem.sentence(2) }
-    category { categories[rand(0..categories.length)] }
+    category { Tag::CATEGORIES.sample }
   end
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe Tag do
   it { should have_many(:users) }
   it { should have_many(:resources) }
-end
 
-describe Tag::CATEGORIES do
-  let!(:tag) { FactoryGirl.create :tag }
-  
-  it { should include(tag.category) }
+  describe 'categories' do
+    let(:tag) { create(:tag) }
+
+    it { expect(Tag::CATEGORIES).to include(tag.category) }
+  end
 end

12d8b0caad80ff558670c3c81b2686b9a982eedf
Lucas Caton <lucascaton@gmail.com>
2013-12-08 15:51:20 +1000

    Adjust in db/seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,6 @@ tag_attributes = File.open("#{Rails.root}/db/seeds/symtoms.txt").each_line.map d
   { name: tag_name, category: 'symtoms' }
 end
 
-Rails.logger.info "Importing #{tag_attributes.count} tags, it could take a while"
+puts "Importing #{tag_attributes.count} tags, it could take a while..."
 
 Tag.create(tag_attributes)

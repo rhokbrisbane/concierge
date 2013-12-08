6acf051c7e21fb46f4e331380bafb96053e58235
Olivier Pichon <olivier@netengine.com.au>
2013-12-08 17:07:21 +1000

    Adds dynamic adding of taggs.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,17 @@ class UsersController < ApplicationController
   load_and_authorize_resource
 
   def show
-    @tags = Tag.all.reject{|tag| @user.tags.include? tag}
+    @tags      = Tag.all.reject{|tag| @user.tags.include? tag}
+    @user_tags = @user.tags
   end
 
   def add_tag
     tag = Tag.find params[:tag_id]
     @user.tags << tag
+    head :no_content
+  end
+
+  def remove_tag
+    @user.tags.find(params[:tag_id]).destroy
   end
 end

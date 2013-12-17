85f4d700853dc3f62fb24ad3e848e2836ff5e9ef
Lucas Caton <lucascaton@gmail.com>
2013-12-17 11:53:09 +1000

    Including more spec

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -6,8 +6,16 @@ describe Tag do
   it { should have_many(:comments) }
 
   describe 'Categories' do
-    let(:tag) { create(:tag) }
+    let(:tag) { create :tag }
 
     it { expect(Tag::CATEGORIES).to include(tag.category) }
+  end
+
+  describe '#to_s' do
+    let(:tag) { build :tag }
+
+    it 'returns the name' do
+      expect(tag.to_s).to eql(tag.name)
+    end
   end
 end

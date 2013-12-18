3ee705aac15ef656bd998cfe30569207211e5488
Lucas Caton <lucascaton@gmail.com>
2013-12-18 10:38:53 +1000

    Including more specs

---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe 'Sign Up', focus: true do
+  context 'as an user' do
+    it 'can sign up' do
+      visit new_user_registration_path
+
+      fill_in 'Email', with: 'email@example.com'
+      fill_in 'Password', with: 'secret1234'
+      fill_in 'Password confirmation', with: 'secret1234'
+
+      expect { click_button 'Sign up' }.to change{ User.count }.by(1)
+
+      expect(page).to have_content('Welcome! You have signed up successfully.')
+      expect(page.current_path).to eql('/')
+    end
+  end
+end

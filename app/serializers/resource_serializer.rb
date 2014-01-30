111f0a2dcadcd6331f231435b14b9f3b21c9fc7d
Lucas Caton <lucascaton@gmail.com>
2014-01-30 15:41:32 +1000

    Fixing ResourceSerializer

--- app/serializers/resource_serializer.rb
+++ app/serializers/resource_serializer.rb
@@ -2,4 +2,10 @@ class ResourceSerializer < ApplicationSerializer
   attributes :id, :name, :category, :url, :phone, :facebook, :twitter, :address
 
   has_one :address, serializer: AddressSerializer
+
+  private
+
+  def category
+    object.resource_category.name
+  end
 end

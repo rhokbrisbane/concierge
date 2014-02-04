aa9b4dd94104a595e7cebedd433f47797a89b426
Lucas Caton <lucascaton@gmail.com>
2014-02-04 14:23:05 +1000

    Updating omniauth-facebook + fixing login

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,15 +27,18 @@ class User < ActiveRecord::Base
     name.presence || email.presence || "User#{id}"
   end
 
-  def self.find_for_facebook_oauth(auth, signed_in_resource = nil)
-    User.where(provider: auth.provider, uid: auth.uid).first || User.create({
-      name:        auth.extra.raw_info.name,
-      provider:    auth.provider,
-      uid:         auth.uid,
-      email:       auth.info.email,
-      image_url:   auth.info.image,
-      password:    Devise.friendly_token[0,20]
-    })
+  def self.find_for_facebook_oauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+      unless user.persisted?
+        user.provider  = auth.provider
+        user.uid       = auth.uid
+        user.email     = auth.info.email
+        user.password  = Devise.friendly_token[0, 20]
+        user.name      = auth.info.name
+        user.image_url = auth.info.image
+        user.save!
+      end
+    end
   end
 
   def self.new_with_session(params, session)

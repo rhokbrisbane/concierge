66882af6888a5d0304b37e848d2d4ddb5cecad6c
ecoologic <erikecoologic@gmail.com>
2015-06-13 17:54:56 +1000

    case insensitive queries

--- app/services/simple_search.rb
+++ app/services/simple_search.rb
@@ -5,20 +5,23 @@ class SimpleSearch
   end
 
   def results
-    OpenStruct.new(
-      resources: resources,
-      pages:     pages
-    )
+    ActiveRecord::Base.transaction do
+      OpenStruct.new(
+        resources: resources,
+        pages:     pages
+      )
+    end
   end
 
   def resources
     resource_sql = %w(name url facebook twitter description).map { |field|
-      "#{field} like :q"
+      "#{field} ilike :q"
     }.join(" or ")
     Resource.where(resource_sql, q: "%#{query}%").includes(:address)
   end
 
   def pages
-    Page.where("title like :q", q: "%#{query}%")
+    binding.pry;"pry";
+    Page.where("title ilike :q", q: "%#{query}%")
   end
 end
